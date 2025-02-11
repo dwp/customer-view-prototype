dc7f4020eead3e309888789fea5f36203841189a
Alison-Lewishill <143614674+Alison-Lewishill@users.noreply.github.com>
2025-02-11 16:45:33 +0000

    Alison update to release 2 - personal details

--- app/routes/release-2.js
+++ app/routes/release-2.js
@@ -7,7 +7,7 @@ var inScope = [];
 var uCBenefit = false;
 var oOScope = [];
 
-router.all('/get-a-proof-of-benefit-letter/v5-research/no-pip/research-set-up/PoB-clear-data', function (req, res) {
+router.all('/get-a-proof-of-benefit-letter/release-2/research-set-up/PoB-clear-data', function (req, res) {
   
   req.session.data = {}
   inScope = [];
@@ -26,7 +26,7 @@ router.all('/get-a-proof-of-benefit-letter/v5-research/no-pip/research-set-up/Po
   
   })
 
-  router.post('/get-a-proof-of-benefit-letter/v5-research/no-pip/research-set-up/benefits-answer', function (req, res) {
+  router.post('/get-a-proof-of-benefit-letter/release-2/research-set-up/benefits-answer', function (req, res) {
 
     var researchSetUpBenefits = req.session.data['researchSetUpBenefits']
   
@@ -69,62 +69,62 @@ router.all('/get-a-proof-of-benefit-letter/v5-research/no-pip/research-set-up/Po
     console.log(oOScope)
     console.log(uCBenefit)
   
-  res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/research-set-up/address');
+  res.redirect('/get-a-proof-of-benefit-letter/release-2/research-set-up/address');
   
    })
 
   
 
 // Drop user if they were not invited to use the service
-router.post('/get-a-proof-of-benefit-letter/v5-research/no-pip/how-did-you-find-out-about-this-service-answer', function (req, res) {
+router.post('/get-a-proof-of-benefit-letter/release-2/how-did-you-find-out-about-this-service-answer', function (req, res) {
 
   var howDidYouFindOutAboutThisService = req.session.data['how-did-you-find-out-about-this-service']
 
 
   if (howDidYouFindOutAboutThisService === "heard-about-on-phone" || howDidYouFindOutAboutThisService === "job-centre")  {
-    res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/go-to-identity-check');
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/go-to-identity-check');
 
   } else {
-    res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/cannot-get-a-proof-of-benefit-letter');
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/cannot-get-a-proof-of-benefit-letter');
   }
 })
 
 // Select which benefit selection page to show based on number of benefits available for user.
-router.all('/get-a-proof-of-benefit-letter/v5-research/no-pip/list-benefits-answer', function (req, res) {
+router.all('/get-a-proof-of-benefit-letter/release-2/list-benefits-answer', function (req, res) {
 
   var researchSetUpBenefits = req.session.data['researchSetUpBenefits']
 
   if (researchSetUpBenefits == [] || researchSetUpBenefits == undefined)  {
-    res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/you-cannot-use-this-service-no-benefits');
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/you-cannot-use-this-service-no-benefits');
     return
 
   } else if ((researchSetUpBenefits.length > 0) && (inScope.length > 0 ))   {
-      res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/select-benefits-you-need-proof-of');
+      res.redirect('/get-a-proof-of-benefit-letter/release-2/select-benefits-you-need-proof-of');
 
   } else if (inScope.length === 0 )  {
-    res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/you-cannot-get-proof-of-benefit-letter');
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/you-cannot-get-proof-of-benefit-letter');
 
   } 
   
 })
 
-router.post('/get-a-proof-of-benefit-letter/v5-research/no-pip/select-benefits-answer', function (req, res) {
+router.post('/get-a-proof-of-benefit-letter/release-2/select-benefits-answer', function (req, res) {
 
   var doYouWantLetterFor = req.session.data['doYouWantLetterFor']
 
     if (doYouWantLetterFor === 'no') {
-      res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/contact-us-for-different-benefit-letter');
+      res.redirect('/get-a-proof-of-benefit-letter/release-2/contact-us-for-different-benefit-letter');
     } 
 
     else {
-      res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/where-we-send-your-letter');
+      res.redirect('/get-a-proof-of-benefit-letter/release-2/where-we-send-your-letter');
     }
 
 })
 
 
 // Drop user if they state their address is incorrect
-router.post('/get-a-proof-of-benefit-letter/v5-research/no-pip/send-letter-to-address-answer', function (req, res) {
+router.post('/get-a-proof-of-benefit-letter/release-2/send-letter-to-address-answer', function (req, res) {
 
   var confirmLetterSend = req.session.data['confirmLetterSend']
 
@@ -132,15 +132,18 @@ router.post('/get-a-proof-of-benefit-letter/v5-research/no-pip/send-letter-to-ad
   // Check if user selected no on single address page
   if (confirmLetterSend == "no")  {
     // Send user to contact us page
-    res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/you-cannot-use-this-service-incorrect-address');
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/you-cannot-use-this-service-incorrect-address');
   }
 
   else {
     // Send user to check answers
-    res.redirect('/get-a-proof-of-benefit-letter/v5-research/no-pip/check-your-answers');
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/check-your-answers');
   }
 
 })
+
+
+
 
 
 module.exports = router;
