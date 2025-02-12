fb6b69115a4b2466068d98ff9d5b3c5a862f5d57
Ali Dashti <ali.dashti2@engineering.digital.dwp.gov.uk>
2025-02-12 12:46:50 +0000

    Northern Ireland flow complete.
    Added link on PoB index page for both Norther Ireland and England/Scotland journeys.
    
    Added warning text on pages with Northern Ireland specific copy.
    
    Change link will only show if more than 1 benefit is selected by user,

--- app/routes/release-2.js
+++ app/routes/release-2.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// * Release - 2
+// * Release - 2 - England and Scotland
 
 var inScope = [];  
 var uCBenefit = false;
@@ -142,6 +142,149 @@ router.post('/get-a-proof-of-benefit-letter/release-2/send-letter-to-address-ans
 
 })
 
+
+
+
+// * Release - 2 - Norther Ireland
+
+var inScope = [];  
+var uCBenefit = false;
+var oOScope = [];
+
+router.all('/get-a-proof-of-benefit-letter/release-2/ni/research-set-up/PoB-clear-data', function (req, res) {
+  
+  req.session.data = {}
+  inScope = [];
+  oOScope = [];
+  uCBenefit = false
+  req.session.data.inScope = inScope;
+  req.session.data.oOScope = oOScope;
+  // delete req.session.data['which-benefits-need-proof'];
+  delete inScope;
+  delete oOScope;
+
+  console.log(inScope);
+  console.log(oOScope);
+  
+  res.redirect("benefits");
+  
+  })
+
+  router.post('/get-a-proof-of-benefit-letter/release-2/ni/research-set-up/benefits-answer', function (req, res) {
+
+    var researchSetUpBenefits = req.session.data['researchSetUpBenefits']
+  
+    inScope = [];
+    oOScope = [];
+    uCBenefit = false
+    delete inScope;
+    delete oOScope;
+    delete uCBenefit
+  
+    for (var i=0; i < researchSetUpBenefits.length; i++) {
+  
+      if (researchSetUpBenefits[i] === 'Bereavement Benefit' ||
+            researchSetUpBenefits[i] === 'Bereavement Support Payment' ||
+            researchSetUpBenefits[i] === 'Carer’s Allowance' ||
+            researchSetUpBenefits[i] === 'Incapacity Benefit' ||
+            researchSetUpBenefits[i] === 'Industrial Injuries Disablement Benefit (IIDB)' ||
+            researchSetUpBenefits[i] === 'Maternity Allowance' ||
+            researchSetUpBenefits[i] === 'Severe Disablement Allowance' ||
+            researchSetUpBenefits[i] === 'Widow’s Benefit'
+          ) {
+            oOScope.push(researchSetUpBenefits[i])
+          } 
+      
+      else if (researchSetUpBenefits[i] === 'Universal Credit') {
+              uCBenefit = true;
+      }
+      
+      else {
+        inScope.push(researchSetUpBenefits[i])
+      }
+    }
+  
+    req.session.data.inScope = inScope;
+    req.session.data.oOScope = oOScope;
+    req.session.data.uCBenefit = uCBenefit;
+  
+  
+    console.log(inScope)
+    console.log(oOScope)
+    console.log(uCBenefit)
+  
+  res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/research-set-up/address');
+  
+   })
+
+  
+
+// Drop user if they were not invited to use the service
+router.post('/get-a-proof-of-benefit-letter/release-2/ni/how-did-you-find-out-about-this-service-answer', function (req, res) {
+
+  var howDidYouFindOutAboutThisService = req.session.data['how-did-you-find-out-about-this-service']
+
+
+  if (howDidYouFindOutAboutThisService === "heard-about-on-phone" || howDidYouFindOutAboutThisService === "job-centre")  {
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/go-to-identity-check');
+
+  } else {
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/cannot-get-a-proof-of-benefit-letter');
+  }
+})
+
+// Select which benefit selection page to show based on number of benefits available for user.
+router.all('/get-a-proof-of-benefit-letter/release-2/ni/list-benefits-answer', function (req, res) {
+
+  var researchSetUpBenefits = req.session.data['researchSetUpBenefits']
+
+  if (researchSetUpBenefits == [] || researchSetUpBenefits == undefined)  {
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/you-cannot-use-this-service-no-benefits');
+    return
+
+  } else if ((researchSetUpBenefits.length > 0) && (inScope.length > 0 ))   {
+      res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/select-benefits-you-need-proof-of');
+
+  } else if (inScope.length === 0 )  {
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/you-cannot-get-proof-of-benefit-letter');
+
+  } 
+  
+})
+
+router.post('/get-a-proof-of-benefit-letter/release-2/ni/select-benefits-answer', function (req, res) {
+
+  var doYouWantLetterFor = req.session.data['doYouWantLetterFor']
+
+    if (doYouWantLetterFor === 'no') {
+      res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/contact-us-for-different-benefit-letter');
+    } 
+
+    else {
+      res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/where-we-send-your-letter');
+    }
+
+})
+
+
+// Drop user if they state their address is incorrect
+router.post('/get-a-proof-of-benefit-letter/release-2/ni/send-letter-to-address-answer', function (req, res) {
+
+  var confirmLetterSend = req.session.data['confirmLetterSend']
+
+
+  // Check if user selected no on single address page
+  if (confirmLetterSend == "no")  {
+    // Send user to contact us page
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/you-cannot-use-this-service-incorrect-address');
+  }
+
+  else {
+    // Send user to check answers
+    res.redirect('/get-a-proof-of-benefit-letter/release-2/ni/check-your-answers');
+  }
+
+})
 
 
 
